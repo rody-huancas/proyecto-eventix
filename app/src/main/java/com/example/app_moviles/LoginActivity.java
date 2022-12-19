e85984910bc22eb7235d0ed37259d9b661057b65
Rody <rodyhuancas.04@gmail.com>
2022-12-19 16:47:48 -0500

    diseño agregado

--- app/src/main/java/com/example/app_moviles/LoginActivity.java
+++ app/src/main/java/com/example/app_moviles/LoginActivity.java
@@ -12,6 +12,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
